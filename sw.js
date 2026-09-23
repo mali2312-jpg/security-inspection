a64e9442e6656317791725cccb9ec0a5722323fb
mali2312-jpg <mali2312@yahoo.de>
2026-09-23 17:50:22 +0200

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
-const CACHE='security-inspection-v8';
-const ASSETS=['./','./index.html','./manifest.webmanifest'];
+const CACHE='security-inspection-v9';
 
 self.addEventListener('install',event=>{
   self.skipWaiting();
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
 });
 
 self.addEventListener('activate',event=>{
   event.waitUntil(
     caches.keys().then(keys=>Promise.all(
-      keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))
+      keys.map(key=>caches.delete(key))
     )).then(()=>self.clients.claim())
   );
 });
@@ -17,10 +15,7 @@ self.addEventListener('activate',event=>{
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET') return;
   event.respondWith(
-    fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request))
+    fetch(event.request, {cache:'no-store'})
+      .catch(()=>caches.match(event.request))
   );
 });
